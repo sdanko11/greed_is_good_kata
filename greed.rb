5a08955246c5456d122d2b4bf84c42e9eda42576
steve <stevedanko1@gmail.com>
2014-03-05 19:29:50 -0500

    refactored a bit

--- greed.rb
+++ greed.rb
@@ -3,16 +3,18 @@ def score(dice)
   score_counted = []
   dice.each do |roll|
     count = dice.count(roll)
-    if roll == 1 && !score_counted.include?(roll)
-      score += (1000 * (count/3))
-      score += (count % 3) * 100
-    elsif roll == 5 && !score_counted.include?(roll)
-      score += (500 * (count/3))
-      score += (count % 3) * 50
-    elsif !score_counted.include?(roll)
-      score += (100 * roll) if count/3 > 0
+    if !score_counted.include?(roll)
+      if roll == 1
+        score += (1000 * (count/3))
+        score += (count % 3) * 100
+      elsif roll == 5
+        score += (500 * (count/3))
+        score += (count % 3) * 50
+      else
+        score += (100 * roll) if (count/3) > 0
+      end
+      score_counted << roll
     end
-    score_counted << roll
   end
   score
 end
